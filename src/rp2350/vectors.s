b14105d69c7fcdd207bb1e8872c27782075f1a88
https-dre <diaso.andre@outlook.com>
2026-03-20 17:23:19 -0300

    feat(vector): add vector table implementation for Cortex-M33

---
+++ src/rp2350/vectors.s
@@ -0,0 +1,29 @@
+.syntax unified
+.cpu cortex-m33
+.thumb
+
+.section .vectors, "a", %progbits
+.global vector_table
+.type vector_Table, %object
+
+.type _start, %function
+.type unhandled, %function
+
+vector_table:
+    .word 0x20082000
+    .word _start
+    .word unhandled
+    .word unhandled
+    .word unhandled
+    .word unhandled
+    .word unhandled
+    .word unhandled
+    .word 0
+    .word 0
+    .word 0
+    .word unhandled
+    .word unhandled
+    .word 0
+    .word unhandled
+    .word unhandled
+.size vector_table, . - vector_table
